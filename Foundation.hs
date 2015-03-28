de455eb266a5e8176b124ce8005aa3706d5ee1fc
Naoto Yokoyama <builtinnya@gmail.com>
2015-03-28 11:36:49 +0900

    Update default-layout
    * Implement logo
    * Implement header layout
    * Use FlatUI

--- Foundation.hs
+++ Foundation.hs
@@ -62,6 +62,7 @@ instance Yesod App where
 
         pc <- widgetToPageContent $ do
             addStylesheet $ StaticR css_bootstrap_css
+            addStylesheet $ StaticR css_flat_ui_css
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
