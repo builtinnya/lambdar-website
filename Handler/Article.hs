e5f3a1b00d333fc853c2a9007f7cb9ffc7f81190
Naoto Yokoyama <builtinnya@gmail.com>
2015-05-02 17:57:49 +0900

    Implement basic article page
    TODOs:
    * Display meta information in more detail at the bottom
    * Display comments at the very bottom

--- Handler/Article.hs
+++ Handler/Article.hs
@@ -5,15 +5,27 @@ import qualified Database.Esqueleto as E
 
 getArticleR :: Text -> Text -> Handler Html
 getArticleR lang slug = do
-  res <- runDB $
+  articles <- runDB $
        E.select
        $ E.from $ \(article, language) -> do
          E.where_ (article  E.^. ArticleSlug  E.==. E.val slug
              E.&&. article  E.^. ArticleLang  E.==. language E.^. LanguageId
              E.&&. language E.^. LanguageSlug E.==. E.val lang)
-         return (article, language)
-  case res of
-   (Entity _ article, Entity _ language) : _ ->
+         return article
+  tags <- runDB $
+       E.select
+       $ E.from $ \(tag_article, tag) -> do
+         E.where_ (tag_article E.^. TagArticleTag         E.==. tag E.^. TagId
+             E.&&. tag_article E.^. TagArticleArticleSlug E.==. E.val slug)
+         return tag
+  langs <- runDB $
+       E.select
+       $ E.from $ \(article, language) -> do
+         E.where_ (article E.^. ArticleSlug E.==. E.val slug
+             E.&&. article E.^. ArticleLang E.==. language E.^. LanguageId)
+         return language
+  case articles of
+   Entity _ article : _ ->
      defaultLayout $ do
        setTitle $ toHtml $ articleTitle article
        $(widgetFile "article")
