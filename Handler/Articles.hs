ed89d006f6fca953a609f6d35f7ed8524d057921
Naoto Yokoyama <builtinnya@gmail.com>
2015-05-13 23:12:04 +0900

    Implement basic articles page
    TODOs:
    * Display tags

---
+++ Handler/Articles.hs
@@ -0,0 +1,18 @@
+module Handler.Articles where
+
+import Import
+import qualified Database.Esqueleto as E
+import qualified Data.Text as T
+
+getArticlesR :: Handler Html
+getArticlesR = do
+  articles <- runDB $
+       E.select
+       $ E.from $ \(article `E.InnerJoin` language) -> do
+         E.on $ article E.^. ArticleLang E.==. language E.^. LanguageId
+         E.orderBy [ E.desc (article E.^. ArticleCreated) ]
+         return (article, language)
+
+  defaultLayout $ do
+    setTitle $ toHtml $ T.pack "Articles"
+    $(widgetFile "articles")
