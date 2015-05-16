661baad975b56f93ad2b92900adca1ebac0da75b
Naoto Yokoyama <builtinnya@gmail.com>
2015-05-16 10:55:33 +0900

    Implement Langs handler: /articles/(en|ja)

--- Application.hs
+++ Application.hs
@@ -34,6 +34,7 @@ import Handler.Common
 import Handler.Home
 import Handler.Article
 import Handler.Articles
+import Handler.Langs
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/Langs.hs
@@ -0,0 +1,21 @@
+module Handler.Langs where
+
+import Import
+import qualified Database.Esqueleto as E
+import qualified Data.Text as T
+
+
+getLangsR :: Text -> Handler Html
+getLangsR slug = do
+  Entity _ lang <- runDB $ getBy404 $ UniqueLanguage slug
+  articles <- runDB $
+       E.select
+       $ E.from $ \(article `E.InnerJoin` language) -> do
+         E.on $ article E.^. ArticleLang E.==. language E.^. LanguageId
+         E.where_ (language E.^. LanguageSlug E.==. E.val slug)
+         E.orderBy [ E.desc (article E.^. ArticleCreated) ]
+         return (article, language)
+
+  defaultLayout $ do
+    setTitle $ toHtml $ T.append ("Articles - " :: Text) (languageName lang)
+    $(widgetFile "langs")
